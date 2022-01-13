bcabbddac4d91ebd7c8ea33ef9f6a22be7632b8c
绪方缘 <947560351@qq.com>
2022-01-14 03:29:12 +0800

    1.2.0

--- src/main/kotlin/Miraisetuplugin.kt
+++ src/main/kotlin/Miraisetuplugin.kt
@@ -1,5 +1,6 @@
 package nya.xfy
 
+import kotlinx.coroutines.launch
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
 import net.mamoe.mirai.event.globalEventChannel
@@ -9,29 +10,30 @@ import net.mamoe.mirai.utils.info
 object Miraisetuplugin : KotlinPlugin(
     JvmPluginDescription(
         id = "nya.xfy.miraisetuplugin",
-        version = "1.1.0",
+        version = "1.2.0",
     )
 ) {
     override fun onEnable() {
         logger.info { "Plugin loaded" }
-        listenerRegister()
+        launch {
+            listener()
+        }
     }
 
-    override fun onDisable() {
-        httpClient.close()
-        super.onDisable()
-    }
-
-    private fun listenerRegister() {
-        Miraisetuplugin.globalEventChannel().subscribeMessages {
-            finding(Regex("""来(\d)*张(.*)色图""")) {
-                Miraisetuplugin.logger.info("正在获取色图")
-                if ((it.groups[2] != null) && (it.groupValues[2] != ""))
-                    Miraisetuplugin.logger.info("keyword: ${it.groupValues[2]}")
-                if (it.groups[1] != null)
-                    Miraisetuplugin.logger.info("num: ${it.groupValues[1]}")
-                Requester(subject).request(it.groups[2]?.value ?: "", it.groups[1]?.value?.toIntOrNull() ?: 1)
+    private fun listener() {
+        try {
+            Miraisetuplugin.globalEventChannel().subscribeMessages {
+                finding(Regex("""来(\d*)张(.*)色图""")) {
+                    Miraisetuplugin.logger.info("正在获取色图")
+                    if ((it.groups[2] != null) && (it.groupValues[2] != ""))
+                        Miraisetuplugin.logger.info("keyword: ${it.groupValues[2]}")
+                    if ((it.groups[1] != null) && (it.groupValues[1] != ""))
+                        Miraisetuplugin.logger.info("num: ${it.groupValues[1].toIntOrNull() ?: 1}")
+                    Requester(subject).request(it.groupValues[2], it.groupValues[1].toIntOrNull() ?: 1)
+                }
             }
+        }catch (e:Throwable){
+            Miraisetuplugin.logger.error(e)
         }
     }
 }
--- src/main/kotlin/Requester.kt
+++ src/main/kotlin/Requester.kt
@@ -1,17 +1,47 @@
 package nya.xfy
 
-import io.ktor.client.request.*
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import net.mamoe.mirai.contact.Contact
 import net.mamoe.mirai.contact.Contact.Companion.sendImage
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import java.io.InputStream
+import java.util.concurrent.TimeUnit
 
 class Requester(private val subject: Contact) {
 
-    private suspend fun getImageStream(resource: String): InputStream =
-        httpClient.get(resource)
+    private val okHttpClient: OkHttpClient = OkHttpClient.Builder().readTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS).connectTimeout(30, TimeUnit.SECONDS).build()
+
+    private fun getResponseCode(url: String): Int =
+        okHttpClient.newCall(Request.Builder().url(url).build()).execute().code
+
+    private fun getImgInputStream(url: String): InputStream =
+        okHttpClient.newCall(Request.Builder().url(url).build()).execute().body!!.byteStream()
+
+    @OptIn(ExperimentalSerializationApi::class)
+    suspend fun request(keyword: String, num: Int) {
+        val url = "https://api.lolicon.app/setu/v2?r18=2&proxy=i.pixiv.re&num=${num}&keyword=${keyword}"
+        if (getResponseCode(url) != 200) {
+            subject.sendMessage("Api好像连不上了，待会再试试看吧？")
+            Miraisetuplugin.logger.error("url: $url")
+            Miraisetuplugin.logger.error("responseCode: ${getResponseCode(url)}")
+        } else {
+            if (num > 5 || num < 1) {
+                if (num < 1)
+                    subject.sendMessage("你真小")
+                if (num > 5)
+                    subject.sendMessage("太大了吧！！怎么看都进不去吧！")
+            } else
+                sendSetu(
+                    Json.decodeFromString(
+                        okHttpClient.newCall(Request.Builder().url(url).build()).execute().body?.string()!!
+                    ), num
+                )
+        }
+    }
 
     private suspend fun sendSetu(response: Response, num: Int) {
         if (response.error != "")
@@ -22,28 +52,13 @@ class Requester(private val subject: Contact) {
             var i = 0
             for (item in response.data) {
                 i++
-                subject.sendImage(getImageStream(item.urls.original))
+                if (getResponseCode(item.urls.original) != 200)
+                    subject.sendMessage("PID: ${item.pid}无法访问，可能已经被删除")
+                else
+                    subject.sendImage(getImgInputStream(item.urls.original))
             }
             if (i < num)
                 subject.sendMessage("哎呀，没有了。。。")
-        }
-    }
-
-    @OptIn(ExperimentalSerializationApi::class)
-    suspend fun request(keyword: String, num: Int) {
-        try {
-            if (num > 5)
-                subject.sendMessage("那也太多了吧？")
-            else if (num < 1)
-                subject.sendMessage("你怎么这么小？")
-            else
-                sendSetu(
-                    Json.decodeFromString(httpClient.get("https://api.lolicon.app/setu/v2?r18=2&proxy=i.pixiv.re&num=${num}&keyword=${keyword}")),
-                    num
-                )
-        } catch (e: Throwable) {
-            Miraisetuplugin.logger.error(e)
-            subject.sendMessage("好像出了点问题，待会再试试吧？")
         }
     }
 }
