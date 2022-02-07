16210371d5c018f00aa3530a583e7d205ec98dee
绪方缘 <947560351@qq.com>
2022-02-08 00:21:19 +0800

    v1.5.0Final

--- src/main/kotlin/MiraiSetuPluginConfig.kt
+++ src/main/kotlin/MiraiSetuPluginConfig.kt
@@ -4,8 +4,9 @@ import net.mamoe.mirai.console.data.AutoSavePluginConfig
 import net.mamoe.mirai.console.data.value
 
 object MiraiSetuPluginConfig : AutoSavePluginConfig("config") {
-    val friendSetuSwitch:Boolean by value(true)
-    val groupSetuSwitch:Boolean by value(true)
+    val readTimeout:Int by value(5)
+    val writeTimeout:Int by value(5)
+    val connectTimeout:Int by value(5)
     val recallTime:Int by value(-1)
     val botOwnerId: List<Long> by value()
 }
