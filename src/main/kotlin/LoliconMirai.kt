acbbadaac77eed6256439d3b6fd0f63bd24aaa07
绪方缘 <947560351@qq.com>
2022-02-24 01:17:34 +0800

    v1.5.3

--- src/main/kotlin/LoliconMirai.kt
+++ src/main/kotlin/LoliconMirai.kt
@@ -1,8 +1,9 @@
 package nya.xfy
 
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.supervisorScope
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
@@ -11,6 +12,7 @@ import net.mamoe.mirai.Bot
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
 import net.mamoe.mirai.contact.Group
+import net.mamoe.mirai.contact.Member
 import net.mamoe.mirai.contact.MemberPermission
 import net.mamoe.mirai.contact.nameCardOrNick
 import net.mamoe.mirai.event.EventPriority
@@ -20,15 +22,15 @@ import net.mamoe.mirai.message.data.ForwardMessage
 import net.mamoe.mirai.message.data.RawForwardMessage
 import net.mamoe.mirai.message.data.buildMessageChain
 import net.mamoe.mirai.utils.ExternalResource.Companion.toExternalResource
+import nya.xfy.LoliconMiraiConfig.botOwnerId
+import nya.xfy.LoliconMiraiConfig.recallTime
 import nya.xfy.LoliconMiraiData.groupR18Map
 import nya.xfy.LoliconMiraiData.groupSetuMap
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import java.net.SocketException
-import java.net.SocketTimeoutException
 import java.util.concurrent.TimeUnit
 
-object LoliconMirai : KotlinPlugin(JvmPluginDescription(id = "nya.xfy.LoliconMirai", version = "1.5.3")) {
+object LoliconMirai : KotlinPlugin(JvmPluginDescription(id = "nya.xfy.LoliconMirai", version = "1.5.3")), CoroutineScope {
 
     private val okHttpClient: OkHttpClient = OkHttpClient.Builder().let {
         it.readTimeout(0, TimeUnit.SECONDS)
@@ -45,40 +47,39 @@ object LoliconMirai : KotlinPlugin(JvmPluginDescription(id = "nya.xfy.LoliconMir
     }
 
     private fun listener() {
-        this.globalEventChannel().subscribeGroupMessages(priority = EventPriority.HIGHEST) {
-            matching(Regex("""来(\d*)张(.*)色图""")) {
-                when (groupSetuMap[subject.id] == true && (it.groupValues[1].toIntOrNull() ?: 1) in 1..5) {
+        this.globalEventChannel().subscribeGroupMessages {
+            finding(Regex("""来点(.*)色图""")) {
+                when (groupSetuMap[subject.id]) {
                     true -> {
-                        logger.info("正在获取${it.groupValues[2]}色图")
-                        when (LoliconMiraiConfig.recallTime in 1..120) {
-                            true -> request(subject, bot, it.groupValues[1].toIntOrNull() ?: 1, it.groupValues[2]).takeIf { it1 -> it1.nodeList.isNotEmpty() }?.let { it2 -> subject.sendMessage(it2).recallIn(LoliconMiraiConfig.recallTime.toLong() * 1000) }
-                            else -> request(subject, bot, it.groupValues[1].toIntOrNull() ?: 1, it.groupValues[2]).takeIf { it1 -> it1.nodeList.isNotEmpty() }?.let { it2 -> subject.sendMessage(it2) }
-                        }
-                        logger.info("${it.groupValues[2]}色图发送完毕")
+                        logger.info("正在获取${it.groupValues[1]}色图")
+                        sendSetu(subject, bot, it.groupValues[1])
+                        logger.info("${it.groupValues[1]}色图发送完毕")
                     }
                     else -> subject.sendMessage("不可以色色！")
                 }
             }
+        }
+        this.globalEventChannel().subscribeGroupMessages(priority = EventPriority.MONITOR) {
             case("开启r18", ignoreCase = true, trim = true) {
-                if (sender.permission > MemberPermission.MEMBER || LoliconMiraiConfig.botOwnerId.contains(sender.id)) {
+                if(getPermission(sender)) {
                     groupR18Map[subject.id] = 2
                     subject.sendMessage("已开启r18")
                 }
             }
             case("关闭r18", ignoreCase = true, trim = true) {
-                if (sender.permission > MemberPermission.MEMBER || LoliconMiraiConfig.botOwnerId.contains(sender.id)) {
+                if(getPermission(sender)) {
                     groupR18Map[subject.id] = 0
                     subject.sendMessage("已关闭r18")
                 }
             }
             case("开启色图", ignoreCase = true, trim = true) {
-                if (sender.permission > MemberPermission.MEMBER || LoliconMiraiConfig.botOwnerId.contains(sender.id)) {
+                if(getPermission(sender)) {
                     groupSetuMap[subject.id] = true
                     subject.sendMessage("已开启色图")
                 }
             }
             case("关闭色图", ignoreCase = true, trim = true) {
-                if (sender.permission > MemberPermission.MEMBER || LoliconMiraiConfig.botOwnerId.contains(sender.id)) {
+                if(getPermission(sender)) {
                     groupSetuMap[subject.id] = false
                     subject.sendMessage("已关闭色图")
                 }
@@ -86,43 +87,35 @@ object LoliconMirai : KotlinPlugin(JvmPluginDescription(id = "nya.xfy.LoliconMir
         }
     }
 
+    private fun getPermission(sender:Member):Boolean=sender.permission > MemberPermission.MEMBER || botOwnerId.contains(sender.id)
+
     @Suppress("NAME_SHADOWING")
     @OptIn(ExperimentalSerializationApi::class)
-    private suspend fun request(subject: Group, bot: Bot, num: Int, keyword: String, mode: String = "tag"): ForwardMessage {
+    private suspend fun sendSetu(subject: Group, bot: Bot, keyword: String, mode: String = "tag") {
         val mutableList = mutableListOf<ForwardMessage.Node>()
-        val response = okHttpClient.newCall(Request.Builder().url("https://api.lolicon.app/setu/v2?r18=${groupR18Map[subject.id]}&proxy=i.pixiv.re&num=${num}&${mode}=${keyword}").build()).execute()
+        val response = okHttpClient.newCall(Request.Builder().url("https://api.lolicon.app/setu/v2?r18=${groupR18Map[subject.id]}&proxy=i.pixiv.re&num=${(1..10).random()}&${mode}=${keyword}").build()).execute()
         when (response.isSuccessful) {
             true -> {
                 val loliconResponse: LoliconResponse = Json.decodeFromString(response.body!!.string())
                 when (loliconResponse.error == "" && loliconResponse.data.isNotEmpty()) {
                     true -> {
-                        if (loliconResponse.data.size < num) mutableList.add(ForwardMessage.Node(bot.id, 0, bot.nameCardOrNick, buildMessageChain { +"关于[${keyword}]的图片只有${loliconResponse.data.size}张" }))
-                        try {
-                            coroutineScope {
-                                for (item in loliconResponse.data) {
-                                    launch {
-                                        val response = okHttpClient.newCall(Request.Builder().url(item.urls.original).build()).execute()
-                                        when (response.isSuccessful) {
-                                            true -> mutableList.add(ForwardMessage.Node(bot.id, 0, bot.nameCardOrNick, buildMessageChain { +subject.uploadImage(response.body!!.byteStream().toExternalResource().toAutoCloseable()) }))
-                                            else -> mutableList.add(ForwardMessage.Node(bot.id, 0, bot.nameCardOrNick, buildMessageChain { +"哎呀，图片失踪了\n${item.urls.original}" }))
-                                        }
-                                        response.close()
+                        supervisorScope  {
+                            for (item in loliconResponse.data) {
+                                launch {
+                                    val response = okHttpClient.newCall(Request.Builder().url(item.urls.original).build()).execute()
+                                    logger.info("PID: ${item.pid}获取中")
+                                    when (response.isSuccessful) {
+                                        true -> mutableList.add(ForwardMessage.Node(bot.id, 0, bot.nameCardOrNick, buildMessageChain { +subject.uploadImage(response.body!!.byteStream().toExternalResource().toAutoCloseable()) }))
+                                        else -> mutableList.add(ForwardMessage.Node(bot.id, 0, bot.nameCardOrNick, buildMessageChain { +"哎呀，图片失踪了\n${item.urls.original}" }))
                                     }
+                                    response.close()
+                                    logger.info("PID: ${item.pid}上传完毕")
                                 }
                             }
-                        } catch (e: IllegalStateException) {
-                            subject.sendMessage("上传部分${keyword}图片失败了，再试试吧？")
-                        } catch (e: SocketTimeoutException) {
-                            subject.sendMessage("请求${keyword}色图时超时了，等等再试试吧？")
-                        } catch (e: SocketException) {
-                            subject.sendMessage("请求${keyword}色图时连接出错了，等等再试试吧？")
-                        } catch (e: Throwable) {
-                            subject.sendMessage("哎呀，出错了。。。")
-                            logger.error(e)
                         }
                     }
                     else -> when (mode == "tag") {
-                        true -> request(subject, bot, num, keyword, "keyword")
+                        true -> sendSetu(subject, bot, keyword, "keyword")
                         else -> subject.sendMessage("你的xp好怪。。。")
                     }
                 }
@@ -130,14 +123,12 @@ object LoliconMirai : KotlinPlugin(JvmPluginDescription(id = "nya.xfy.LoliconMir
             else -> subject.sendMessage("无法连接到LoliconApi")
         }
         response.close()
-        return RawForwardMessage(mutableList).render(object : ForwardMessage.DisplayStrategy {
-            override fun generateTitle(forward: RawForwardMessage): String {
-                return when (keyword == "") {
+        subject.sendMessage(RawForwardMessage(mutableList).render(object : ForwardMessage.DisplayStrategy {
+            override fun generateTitle(forward: RawForwardMessage) = when (keyword == "") {
                     true -> "群聊的聊天记录"
                     else -> keyword
                 }
-            }
-        })
+        })).takeIf { recallTime in 1..120 }?.recallIn((recallTime*1000).toLong())
     }
 
     @Serializable
