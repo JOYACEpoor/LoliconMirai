1fd0417598b33a4a974e1891abfe1150732ed796
绪方缘 <947560351@qq.com>
2022-03-27 02:06:05 +0800

    v1.6.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     val kotlinVersion = "1.5.30"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
-    id("net.mamoe.mirai-console") version "2.9.2"
+    id("net.mamoe.mirai-console") version "2.11.0-M1"
 }
 
 group = "nya.xfy"
