0abdbe6b152c03527ae9797a4234e04b28eca0f5
Rothes <449181985@qq.com>
2022-04-22 22:23:07 +0800

    优化指令用法

--- src/main/kotlin/configs/ReplyConfig.kt
+++ src/main/kotlin/configs/ReplyConfig.kt
@@ -8,8 +8,10 @@ object ReplyConfig : AutoSavePluginConfig("ReplyConfig") {
     val refuseReply: String by value("不可以色色！")
     val setuOnReply: String by value("已开启色图")
     val setuOffReply: String by value("已关闭色图")
-    val r18OnReply: String by value("已开启r18")
-    val r18OffReply: String by value("已关闭r18")
+    val r18OffReply: String by value("已关闭 R18")
+    val r18OnReply: String by value("已开启 R18")
+    val r18MixedReply: String by value("已开始 R18 混合模式")
+    val invalidR18Input: String by value("输入无效. 支持参数如下:\n0 - 关闭 R18 模式\n1 - 开始 R18 模式\n2 - R18 混合模式")
     val noResultReply by value("你的xp好怪。。。")
     val connectionFailureReply by value("连接Api失败")
 }
